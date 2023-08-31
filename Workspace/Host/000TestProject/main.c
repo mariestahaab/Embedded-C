ee9a31191f7f947643f6e208540880e4393da97b
MariesTahaab <mariestahaab@gmail.com>
2023-08-31 01:29:30 +0100

    Environment setup complete.

---
+++ Workspace/Host/000TestProject/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 30, 2023
+ *      Author: mariestahaab
+ */
+
+#include <stdio.h>
+
+int main(void){
+
+	printf("Hello World!");
+
+	getchar();
+
+	return 0;
+}
